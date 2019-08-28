50642da12f2afe3165f7ea10b8d34591f67ebf9e
Nicholas Le <nle031@west-mec.org>
2019-08-28 16:11:06 -0700

    Updated html, css, and js
    Centered the text of all sections and removed left and right padding. Added js to change the color of the nav links on mouse over {Doesn't currently work}

--- main.js
+++ main.js
@@ -8,4 +8,16 @@
 
 
 
-// form
+// nav color changer on hover
+window.addEventListener("load", colorGenerator);
+
+
+function colorGenerator() {
+   let colorChanger= Math.floor(Math.random() * 16777215).toString(16);
+   const navLink = document.getElementsByClassName("nav-link");
+   navLink.style.color = "#" + colorChanger;
+   navLink.hover = colorGenerator;
+}
+
+
+
