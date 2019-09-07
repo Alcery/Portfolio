28ba7c56c4ecae93d4d62dff97e5382f36cd6817
Nicholas Le <nle031@west-mec.org>
2019-09-06 17:27:45 -0700

    Added picture and updated skills section
    Added skills to the skill section (Just need to add descriptions to those skills)
    added a photo to the about section
    reduced padding to get a smoother scroll
    changed the height for sections to remove white space
    fixed javascript regex

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-"use strict";
-
-
 // form validation
 function validate() {
    const contactname = document.contactform.contactname;
@@ -25,12 +22,12 @@ function validate() {
    } else {
       return true;
    }
-};
+}
 
 // Email regex
 function ValidateEmail(inputText)
 {
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
    if(inputText.value.match(mailformat))
    {
    document.contactform.contactemail.focus();
@@ -69,56 +66,56 @@ let colorChanger = Math.floor(Math.random() * 16777215).toString(16);
 console.log(colorChanger);
 // This event will activate every time the cursor
 // is moved onto a nav link
-navLink[0].addEventListener("mouseover", function (event) {
+navLink[0].addEventListener("mouseover", event => {
    console.log(navLink);
    // highlight the mouseenter target
-   event.target.style.color = "#" + colorChanger;
+   event.target.style.color = `#${colorChanger}`;
 
    // reset the color after a short delay
-   setTimeout(function () {
+   setTimeout(() => {
       event.target.style.color = "";
    }, 700);
 }, false);
 
 // This event will activate every time the cursor
 // is moved over a nav link
-navLink[1].addEventListener("mouseover", function (event) {
+navLink[1].addEventListener("mouseover", event => {
    // highlight the mouseover target
-   event.target.style.color = "#" + colorChanger;
+   event.target.style.color = `#${colorChanger}`;
 
    // reset the color after a short delay
-   setTimeout(function () {
+   setTimeout(() => {
       event.target.style.color = "";
    }, 700);
 }, false);
-navLink[2].addEventListener("mouseover", function (event) {
+navLink[2].addEventListener("mouseover", event => {
    // highlight the mouseover target
-   event.target.style.color = "#" + colorChanger;
+   event.target.style.color = `#${colorChanger}`;
 
    // reset the color after a short delay
-   setTimeout(function () {
+   setTimeout(() => {
       event.target.style.color = "";
    }, 700);
 }, false);
-navLink[3].addEventListener("mouseover", function (event) {
+navLink[3].addEventListener("mouseover", event => {
    // highlight the mouseover target
-   event.target.style.color = "#" + colorChanger;
+   event.target.style.color = `#${colorChanger}`;
 
    // reset the color after a short delay
-   setTimeout(function () {
+   setTimeout(() => {
       event.target.style.color = "";
    }, 700);
 }, false);
-navLink[4].addEventListener("mouseover", function (event) {
+navLink[4].addEventListener("mouseover", event => {
    // highlight the mouseover target
-   event.target.style.color = "#" + colorChanger;
+   event.target.style.color = `#${colorChanger}`;
 
    // reset the color after a short delay
-   setTimeout(function () {
+   setTimeout(() => {
       event.target.style.color = "";
    }, 700);
 }, false);
 
 
 // Nav Array (used later)
-var nav = ["About", "Education", "Skills", "Services", "Contact"];
+const nav = ["About", "Education", "Skills", "Services", "Contact"];
